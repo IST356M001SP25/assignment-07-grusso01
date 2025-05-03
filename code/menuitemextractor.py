b080f15039eff1f6e7d988b0ea121cee41584c00
grusso01 <grusso01@syr.edu>
2025-05-03 13:33:29 -0400

    First commit of the assignment

--- code/menuitemextractor.py
+++ code/menuitemextractor.py
@@ -7,13 +7,34 @@ else:
 
 
 def clean_price(price:str) -> float:
-    pass # TODO  replace with code
+    price = price.replace("$", "")
+    price = price.replace(",", "")
+    return float(price)
 
 def clean_scraped_text(scraped_text: str) -> list[str]:
-    pass # TODO  replace with code
+    items = scraped_text.split("\n")
+    clean = []
+    for item in items:
+        if item in ["S","V","GS","P"]:
+            continue
+        if item.startswith("NEW"):
+            continue
+        if len(item.strip()) == 0:
+            continue
+
+        clean.append(item)
+    return clean
 
 def extract_menu_item(title:str, scraped_text: str) -> MenuItem:
-    pass # TODO  replace with code
+    clean_items = clean_scraped_text(scraped_text)
+    item = MenuItem(category=title, name="", price=0.0, description="")
+    item.name = clean_items[0]
+    item.price = clean_price(clean_items[1])
+    if len(clean_items) > 2:
+        item.description = clean_items[2]
+    else:
+        item.description = "No description available."
+    return item
 
 
 
